d7273680e965c4966c6c773133f797908b5fc15f
Ignacio Andres Navarrete <elignacio2604@gmail.com>
2025-12-08 15:24:06 -0300

    15vo deploy

--- backend/apps/authentication/urls.py
+++ backend/apps/authentication/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'authentication'
 
 urlpatterns = [
-    path('register', views.register_view, name='register'),
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('me', views.me_view, name='me'),
-    path('google', views.google_auth_view, name='google'),
-    path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('me/', views.me_view, name='me'),
+    path('google/', views.google_auth_view, name='google'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
